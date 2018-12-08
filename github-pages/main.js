519d365b6640fda1bb19c3757bd3bf1959c17041
Maor765 <maor765@gmail.com>
2018-12-08 12:27:54 +0200

    Auto-generated commit

---
+++ github-pages/main.js
@@ -0,0 +1,194 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error('Cannot find module "' + req + '".');
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!--Navbar-->\n<nav class=\"navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar\">\n  <div class=\"container\">\n    <a class=\"navbar-brand\" href=\"#\">Navbar</a>\n    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n      <span class=\"navbar-toggler-icon\"></span>\n    </button>\n    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n      <ul class=\"navbar-nav mr-auto\">\n        <li class=\"nav-item active\">\n          <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\">Features</a>\n        </li>\n        <li class=\"nav-item\">\n          <a class=\"nav-link\" href=\"#\">Pricing</a>\n        </li>\n        <li class=\"nav-item btn-group\">\n          <a class=\"nav-link dropdown-toggle\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown \n                            </a>\n          <div class=\"dropdown-menu dropdown-primary\" aria-labelledby=\"navbarDropdownMenuLink\">\n            <a class=\"dropdown-item\" href=\"#\">Action</a>\n            <a class=\"dropdown-item\" href=\"#\">Another action</a>\n            <a class=\"dropdown-item\" href=\"#\">Something else here</a>\n          </div>\n        </li>\n      </ul>\n      <form class=\"form-inline\">\n        <input class=\"form-control mr-sm-2\" type=\"text\" placeholder=\"Search\" aria-label=\"Search\">\n      </form>\n    </div>\n  </div>\n</nav>\n<!--/.Navbar-->\n<!--Carousel Wrapper-->\n<div id=\"carousel-example-3\" class=\"carousel slide carousel-fade white-text\" data-ride=\"carousel\" data-interval=\"false\">\n  <!--Indicators-->\n  <ol class=\"carousel-indicators\">\n    <li data-target=\"#carousel-example-3\" data-slide-to=\"0\" class=\"active\"></li>\n    <li data-target=\"#carousel-example-3\" data-slide-to=\"1\"></li>\n    <li data-target=\"#carousel-example-3\" data-slide-to=\"2\"></li>\n  </ol>\n  <!--/.Indicators-->\n  <!--Slides-->\n  <div class=\"carousel-inner\" role=\"listbox\">\n    <!-- First slide -->\n    <div class=\"carousel-item active view hm-black-light\" style=\"background-image: url('https://mdbootstrap.com/img/Photos/Others/img (43).jpg'); background-repeat: no-repeat; background-size: cover;\">\n      <!-- Caption -->\n      <div class=\"full-bg-img flex-center white-text\">\n        <ul class=\"animated fadeInUp col-md-12\">\n          <li>\n            <h1 class=\"h1-responsive flex-item font-bold\">Material Design for Bootstrap 4</h1>\n            <li>\n              <p class=\"flex-item\">The most powerful and free UI KIT for Bootstrap</p>\n            </li>\n            <li>\n              <a target=\"_blank\" href=\"https://mdbootstrap.com/getting-started/\" class=\"btn btn-primary btn-lg flex-item\" rel=\"nofollow\">Sign up!</a>\n            </li>\n            <li>\n              <a target=\"_blank\" href=\"https://mdbootstrap.com/material-design-for-bootstrap/\" class=\"btn btn-secondary btn-lg flex-item\" rel=\"nofollow\">Learn more</a>\n            </li>\n        </ul>\n      </div>\n      <!-- /.Caption -->\n    </div>\n    <!-- /.First slide -->\n    <!-- Second slide -->\n    <div class=\"carousel-item view hm-black-light\" style=\"background-image: url('https://mdbootstrap.com/img/Photos/Others/img%20(40).jpg'); background-repeat: no-repeat; background-size: cover;\">\n      <!-- Caption -->\n      <div class=\"full-bg-img flex-center white-text\">\n        <ul class=\"animated fadeInUp col-md-12\">\n          <li>\n            <h1 class=\"h1-responsive\">Lots of tutorials at your disposal</h1>\n          </li>\n          <li>\n            <p class=\"my-4\">And all of them are FREE!</p>\n          </li>\n          <li>\n            <a target=\"_blank\" href=\"https://mdbootstrap.com/bootstrap-tutorial/\" class=\"btn btn-primary btn-lg\" rel=\"nofollow\">Start learning</a>\n          </li>\n        </ul>\n      </div>\n      <!-- /.Caption -->\n    </div>\n    <!-- /.Second slide -->\n    <!-- Third slide -->\n    <div class=\"carousel-item view hm-black-light\" style=\"background-image: url('https://mdbootstrap.com/img/Photos/Horizontal/Nature/full%20page/img%20(24).jpg'); background-repeat: no-repeat; background-size: cover;\">\n      <!-- Caption -->\n      <div class=\"full-bg-img flex-center white-text\">\n        <ul class=\"animated fadeInUp col-md-12\">\n          <li>\n            <h1 class=\"h1-responsive\">Visit our support forum</h1></li>\n          <li>\n            <p class=\"my-4\">Our community can help you with any question</p>\n          </li>\n          <li>\n            <a target=\"_blank\" href=\"https://mdbootstrap.com/forums/forum/support/\" class=\"btn btn-default btn-lg\" rel=\"nofollow\">Support forum</a>\n          </li>\n        </ul>\n      </div>\n      <!-- /.Caption -->\n    </div>\n    <!-- /.Third slide -->\n  </div>\n  <!--/.Slides-->\n  <!--Controls-->\n  <a class=\"carousel-control-prev\" href=\"#carousel-example-3\" role=\"button\" data-slide=\"prev\">\n            <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>\n            <span class=\"sr-only\">Previous</span>\n        </a>\n  <a class=\"carousel-control-next\" href=\"#carousel-example-3\" role=\"button\" data-slide=\"next\">\n            <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>\n            <span class=\"sr-only\">Next</span>\n        </a>\n  <!--/.Controls-->\n</div>\n<!--/.Carousel Wrapper-->\n<!--/.Main layout-->\n<!--Footer-->\n<footer class=\"page-footer center-on-small-only\">\n  <!--Footer links-->\n  <div class=\"container-fluid\">\n    <div class=\"row\">\n      <!--First column-->\n      <div class=\"col-lg-3 col-md-6 ml-auto\">\n        <h5 class=\"title mb-3\"><strong>About material design</strong></h5>\n        <p>Material Design (codenamed Quantum Paper) is a design language developed by Google.</p>\n        <p>Material Design for Bootstrap (MDB) is a powerful Material Design UI KIT for most popular HTML, CSS, and JS framework - Bootstrap.</p>\n      </div>\n      <!--/.First column-->\n      <hr class=\"w-100 clearfix d-sm-none\">\n      <!--Second column-->\n      <div class=\"col-lg-2 col-md-6 ml-auto\">\n        <h5 class=\"title mb-3\"><strong>First column</strong></h5>\n        <ul>\n          <li>\n            <a href=\"#!\">Link 1</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 2</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 3</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 4</a>\n          </li>\n        </ul>\n      </div>\n      <!--/.Second column-->\n      <hr class=\"w-100 clearfix d-sm-none\">\n      <!--Third column-->\n      <div class=\"col-lg-2 col-md-6 ml-auto\">\n        <h5 class=\"title mb-3\"><strong>Second column</strong></h5>\n        <ul>\n          <li>\n            <a href=\"#!\">Link 1</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 2</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 3</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 4</a>\n          </li>\n        </ul>\n      </div>\n      <!--/.Third column-->\n      <hr class=\"w-100 clearfix d-sm-none\">\n      <!--Fourth column-->\n      <div class=\"col-lg-2 col-md-6 ml-auto\">\n        <h5 class=\"title mb-3\"><strong>Third column</strong></h5>\n        <ul>\n          <li>\n            <a href=\"#!\">Link 1</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 2</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 3</a>\n          </li>\n          <li>\n            <a href=\"#!\">Link 4</a>\n          </li>\n        </ul>\n      </div>\n      <!--/.Fourth column-->\n    </div>\n  </div>\n  <!--/.Footer links-->\n  <hr>\n  <!--Call to action-->\n  <div class=\"call-to-action\">\n    <h4 class=\"mb-5\">Material Design for Bootstrap</h4>\n    <ul>\n      <li>\n        <h5>Get our UI KIT for free</h5></li>\n      <li><a target=\"_blank\" href=\"https://mdbootstrap.com/getting-started/\" class=\"btn btn-primary\" rel=\"nofollow\">Sign up!</a></li>\n      <li><a target=\"_blank\" href=\"https://mdbootstrap.com/material-design-for-bootstrap/\" class=\"btn btn-default\" rel=\"nofollow\">Learn more</a></li>\n    </ul>\n  </div>\n  <!--/.Call to action-->\n  <!--Copyright-->\n  <div class=\"footer-copyright\">\n    <div class=\"container-fluid\">\n      © 2015 Copyright: <a href=\"https://www.MDBootstrap.com\"> MDBootstrap.com </a>\n    </div>\n  </div>\n  <!--/.Copyright-->\n</footer>\n<!--/.Footer-->"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    AppComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\MaorH\Documents\Visual Studio 2015\Projects\@Sample projects @\github-pages\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
